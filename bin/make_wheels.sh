031eefb3a5795f44ab63e0a967b5d1512d35be95
Pablo Acosta-Serafini <pmasdev@gmail.com>
2018-12-10 20:35:30 -0500

    Wheel-making fixes

--- bin/make_wheels.sh
+++ bin/make_wheels.sh
@@ -4,17 +4,29 @@
 # Copyright (c) 2013-2018 Pablo Acosta-Serafini
 # See LICENSE for details
 
+set -e
+
 source "$(dirname "${BASH_SOURCE[0]}")/functions.sh"
 pkg_dir=$(dirname "$(current_dir "${BASH_SOURCE[0]}")")
-sbin_dir=${pkg_dir}/sbin
-source "${sbin_dir}"/ipath.sh
+
 cwd=${PWD}
+cpath="${PATH}"
+finish() {
+    cp -rf "${pkg_dir}/MANIFEST.in.test" "${pkg_dir}/MANIFEST.in"
+    export PATH="${cpath}"
+    cd "${cwd}" || exit 1
+}
+
+trap finish EXIT ERR SIGINT
+
+cp -rf "${pkg_dir}/MANIFEST.in.wheel" "${pkg_dir}/MANIFEST.in"
+bin_dir=${pkg_dir}/bin
 echo "pkg_dir: ${pkg_dir}"
-echo "sbin_dir: ${sbin_dir}"
+echo "bin_dir: ${bin_dir}"
 cd "${pkg_dir}" || exit 1
-vers=(2.7 3.5 3.6)
+vers=(2.7 3.5 3.6 3.7)
 for ver in "${vers[@]}"; do
-    "${sbin_dir}/cprint.sh" line cyan "Building Python ${ver} wheel"
+    export PATH=${HOME}/python/python${ver}/bin:${PATH}
+    "${bin_dir}/cprint.sh" line cyan "Building Python ${ver} wheel"
     "${HOME}/python/python${ver}/bin/python${ver}" setup.py bdist_wheel --python-tag py"${ver/./}"
 done
-cd "${cwd}" || exit 1
