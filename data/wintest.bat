59a14279fb81330ae29fa3d3f2021a67eaf81132
Pablo Acosta-Serafini <pmasdev@gmail.com>
2018-12-31 16:39:48 -0500

    Linting

--- data/wintest.bat
+++ data/wintest.bat
@@ -36,18 +36,15 @@ REM # Set up environment variables
 REM ###
 REM >>> VERBATIM
 SET
-SET PYTHONCMD=python
-SET PYTESTCMD=pytest
 SET SHELLCHECK_TEST_ENV=1
-SET PIPCMD=%PYTHONCMD% -m pip
-%PYTHONCMD% -c "import os; print(os.path.basename(os.getcwd()))" > pkg_name.txt
+python -c "import os; print(os.path.basename(os.getcwd()))" > pkg_name.txt
 SET /p PKG_NAME=<pkg_name.txt
-%PYTHONCMD% -c "import os; print(os.path.basename(os.getcwd()).split('-')[0])" > pkg_name_alt.txt
+python -c "import os; print(os.path.basename(os.getcwd()).split('-')[0])" > pkg_name_alt.txt
 SET /p PKG_NAME_ALT=<pkg_name_alt.txt
-%PYTHONCMD% -c "import os, pip; print(os.path.dirname(os.path.realpath(pip.__path__[0])))" > python_site_packages_dir.txt
+python -c "import os, pip; print(os.path.dirname(os.path.realpath(pip.__path__[0])))" > python_site_packages_dir.txt
 SET /p PYTHON_SITE_PACKAGES=<python_site_packages_dir.txt
 SET REPO_DIR=%CD%
-%PYTHONCMD% -c "from __future__ import print_function; import sys; print(sys.prefix)" > extra_dir.txt
+python -c "from __future__ import print_function; import sys; print(sys.prefix)" > extra_dir.txt
 SET /p EXTRA_DIR_BASE=<extra_dir.txt
 SET EXTRA_DIR=%EXTRA_DIR_BASE%\share\%PKG_NAME%
 SET SBIN_DIR=%EXTRA_DIR%\bin
@@ -60,9 +57,6 @@ SET COV_FILE=%SOURCE_DIR%\.coveragerc_ci_%INTERP%
 SET CITMP=%REPO_DIR%\CITMP
 IF NOT EXIST "%CITMP%" MKDIR %CITMP%
 SET PYLINT_PLUGINS_DIR=%EXTRA_DIR%\pylint_plugins
-ECHO PYTHONCMD=%PYTHONCMD%
-ECHO PIPCMD=%PIPCMD%
-ECHO PYTESTCMD=%PYTESTCMD%
 ECHO INTERP=%INTERP%
 ECHO PKG_NAME=%PKG_NAME%
 ECHO PKG_NAME_ALT=%PKG_NAME_ALT%
@@ -80,8 +74,8 @@ REM ###
 REM # Install package dependencies
 REM ###
 CD %REPO_DIR%
-%PIPCMD% install codecov
-%PIPCMD% freeze
+python -m pip install codecov
+python -m pip freeze
 REM ###
 REM # Install shellcheck and bash binaries
 REM ###
@@ -103,16 +97,16 @@ REM # Install package
 REM ###
 REM >>> VERBATIM
 TYPE %REPO_DIR%\MANIFEST.in
-%PYTHONCMD% setup.py sdist --formats=zip
+python setup.py sdist --formats=zip
 TIMEOUT /t 5
 REM # Change directory away from repository, otherwise pip does not install package
 SET SHELLCHECK_CI_ENV=1
-%PYTHONCMD% -c "import os, sys; sys.path.append(os.path.realpath('.'));import setup; print(setup.__version__)" > version.txt
+python -c "import os, sys; sys.path.append(os.path.realpath('.'));import setup; print(setup.__version__)" > version.txt
 SET SHELLCHECK_CI_ENV=
 SET /p PKG_VERSION=<version.txt
 ECHO PKG_VERSION=%PKG_VERSION%
 CD %PYTHON_SITE_PACKAGES%
-%PIPCMD% install --upgrade %REPO_DIR%\dist\%PKG_NAME%-%PKG_VERSION%.zip
+python -m pip install --upgrade %REPO_DIR%\dist\%PKG_NAME%-%PKG_VERSION%.zip
 REM # Write coverage configuration file
 ECHO # .coveragerc_CI to control coverage.py during Appveyor runs > %COV_FILE%
 ECHO [report] >> %COV_FILE%
@@ -125,7 +119,7 @@ ECHO omit = %SOURCE_DIR%\websupport\* >> %COV_FILE%
 ECHO [xml] >> %COV_FILE%
 ECHO output = %RESULTS_DIR%\codecoverage\coverage.xml >> %COV_FILE%
 REM ###
-%PYTHONCMD% %SBIN_DIR%\coveragerc_manager.py 'ci' 1 %INTERP% %PYTHON_SITE_PACKAGES%
+python %SBIN_DIR%\coveragerc_manager.py 'ci' 1 %INTERP% %PYTHON_SITE_PACKAGES%
 TYPE %COV_FILE%
 REM ###
 REM # Change to tests sub-directory to mimic Tox conditions
@@ -138,7 +132,7 @@ REM ###
 REM # Run tests
 REM ###
 REM >>> VERBATIM
-%PYTHONCMD% %SBIN_DIR%\check_files_compliance.py -tps -d %SOURCE_DIR% -m %EXTRA_DIR%
+python %SBIN_DIR%\check_files_compliance.py -tps -d %SOURCE_DIR% -m %EXTRA_DIR%
 CD %SOURCE_DIR%
 for %%i in (*.py) do pylint --rcfile=%EXTRA_DIR%\.pylintrc --ignore=websupport -f text --ignore=websupport -r no %%i
 CD %SBIN_DIR%
@@ -148,12 +142,12 @@ for /r %%i in (*.py) do pylint --rcfile=%EXTRA_DIR%\.pylintrc -f text -r no %%i
 REM ###
 CD %EXTRA_DIR%\tests
 SET DODOCTEST=1
-%PYTESTCMD% --collect-only --doctest-glob="*.rst" %EXTRA_DIR%\docs > doctest.log 2>&1 || SET DODOCTEST=0
-IF %DODOCTEST%==1 %PYTESTCMD% --doctest-glob="*.rst" %EXTRA_DIR%\docs
+pytest --collect-only --doctest-glob="*.rst" %EXTRA_DIR%\docs > doctest.log 2>&1 || SET DODOCTEST=0
+IF %DODOCTEST%==1 pytest --doctest-glob="*.rst" %EXTRA_DIR%\docs
 SET DODOCTEST=1
-%PYTESTCMD% --collect-only --doctest-modules %SOURCE_DIR% > doctest.log 2>&1 || SET DODOCTEST=0
-IF %DODOCTEST%==1 %PYTESTCMD% --doctest-modules %SOURCE_DIR%
-%PYTESTCMD% --cov-config %COV_FILE% --cov %SOURCE_DIR% --cov-report term
+pytest --collect-only --doctest-modules %SOURCE_DIR% > doctest.log 2>&1 || SET DODOCTEST=0
+IF %DODOCTEST%==1 pytest --doctest-modules %SOURCE_DIR%
+pytest --cov-config %COV_FILE% --cov %SOURCE_DIR% --cov-report term
 
 REM <<< VERBATIM
 REM on_failure:
