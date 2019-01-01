a6ecc10aab1889bfe0cf8de02d3d3d1a4d848132
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-01-01 16:16:20 -0500

    More failure verbosity

--- tests/test_shellcheck.py
+++ tests/test_shellcheck.py
@@ -105,10 +105,22 @@ def test_shellcheck():
     ]
     flag = any([(ref_lines_1 == act_lines), (ref_lines_2 == act_lines)])
     if not flag:
-        print("Reference 1")
-        print(ref_lines_1)
-        print("Reference 2")
-        print(ref_lines_2)
         print("Actual")
         print(act_lines)
+        print("Reference 1")
+        print(ref_lines_1)
+        print(ref_lines_1 == act_lines)
+        print('---')
+        for line_1, line_2 in zip(ref_lines_2, act_lines):
+            print(line_1)
+            print(line_2)
+        print('---')
+        print("Reference 2")
+        print(ref_lines_2)
+        print(ref_lines_2 == act_lines)
+        print('---')
+        for line_1, line_2 in zip(ref_lines_2, act_lines):
+            print(line_1)
+            print(line_2)
+        print('---')
     assert flag
