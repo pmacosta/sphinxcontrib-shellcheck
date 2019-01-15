4d8729170f66806ae9f21846e54305188d0c36b0
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-01-15 09:38:33 -0500

    Configure git repo with appropriate author information

--- .hooks/setup-git-hooks.sh
+++ .hooks/setup-git-hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1090,SC1091
 # Functions
 current_dir() {
     local sdir="$1"
@@ -17,6 +18,7 @@ current_dir() {
 ### Unofficial strict mode
 set -euo pipefail
 IFS=$'\n\t'
+###
 cwd=${PWD}
 print_banner=0
 finish() {
@@ -28,16 +30,43 @@ finish() {
     exit "${1:-1}"
 }
 sdir=$(current_dir "${BASH_SOURCE[0]}")
+# Configure Git user name and email
+email=0
+personal_repo=0
+cfg_fname="${sdir}/repo-cfg.sh"
+if [ -f "${cfg_fname}" ]; then
+    source "${sdir}/repo-cfg.sh"
+else
+    echo -e "\tConfiguration file not found, using defaults"
+fi
+if [ "${email}" == 1 ]; then
+    if [ "${personal_repo}" == 1 ]; then
+        if [ "${PERSONAL_NAME}" != "" ] && [ "${PERSONAL_EMAIL}" != "" ]; then
+            echo "Configuring personal Git author information"
+            git config user.name "${PERSONAL_NAME}"
+            git config user.email "${PERSONAL_EMAIL}"
+        fi
+    else
+        if [ "${WORK_NAME}" != "" ] && [ "${WORK_EMAIL}" != "" ]; then
+            echo "Configuring work Git author information"
+            git config user.name "${WORK_NAME}"
+            git config user.email "${WORK_EMAIL}"
+        fi
+    fi
+fi
+finish 0
 repo_dir=$(dirname "${sdir}")
 # Use pre-commit framework if possible
 if [ -f "${repo_dir}/.pre-commit-config.yaml" ]; then
     if which pre-commit &> /dev/null; then
         cd "${repo_dir}" || exit 1
+        echo "Setting up pre-commit framework"
         pre-commit install
         finish 0
     fi
 fi
 # Default to legacy shell-based framework
+echo "Setting up shell pre-commit hook"
 git_hooks_dir=${repo_dir}/.git/hooks
 hooks=(pre-commit)
 cd "${git_hooks_dir}" || exit 1
