c8d8138fbced7d7902042ec17fcc290ab1538dda
Pablo Acosta-Serafini <pmasdev@gmail.com>
2020-03-29 06:07:00 -0400

    Updated ending year of copyright notice. Auto-detect versions of Python available for testing.

--- bin/rtest.sh
+++ bin/rtest.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 # rtest.sh
-# Copyright (c) 2018-2019 Pablo Acosta-Serafini
+# Copyright (c) 2018-2020 Pablo Acosta-Serafini
 # See LICENSE for details
 
+
+sdir=$(dirname "${BASH_SOURCE[0]}")
+# shellcheck disable=SC1090,SC1091,SC2024
+source "${sdir}/functions.sh"
 opath=${PATH}
-export PATH=${HOME}/python/python2.7/bin:${HOME}/python/python3.5/bin:${HOME}/python/python3.6/bin:${HOME}/python/python3.7/bin:${PATH}
-tox
-export PATH=${opath}
+### Unofficial strict mode
+set -euo pipefail
+IFS=$'\n\t'
+finish() {
+    export PATH=${opath}
+}
+trap finish EXIT ERR SIGINT
+pyvers=(3.5 3.6 3.7 3.8)
+for pyver in ${pyvers[*]}; do
+    pdir="${HOME}/python/python${pyver}/bin"
+    if [ -d "${pdir}" ]; then
+        export PATH=${pdir}:${PATH}
+    fi
+done
+# shellcheck disable=SC2068
+PKG_NAME=sphinxcontrib-shellcheck tox -- $@
